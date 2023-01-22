fc78ea214e9bfb069c95e325201796caa10883dd
carlstep <carlstep333@me.com>
2023-01-22 16:50:05 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/provider_tutorial_valuenotifier_andrea/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.provider_tutorial_valuenotifier_andrea
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'counter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Provider ValueNotifier example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChangeNotifierProvider<ValueNotifier<int>>(
+          create: ((context) => ValueNotifier<int>(0)),
+          child: const MyHomePage(title: 'Provider ValueNotifier example')),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<ValueNotifier<int>>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Consumer<ValueNotifier<int>>(
+              builder: (context, value, child) => Text(
+                '${counter.value}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => counter.value++,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
